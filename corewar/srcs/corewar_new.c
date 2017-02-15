49449dfb5d9ca864c01b55201b5c7a6ac47681e7
Etienne ROBERT <etrobert@e3r10p19.42.fr>
2017-02-15 19:47:49 +0100

    Minor

--- corewar/srcs/corewar_new.c
+++ corewar/srcs/corewar_new.c
@@ -6,19 +6,19 @@
 /*   By: etrobert <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/14 21:00:58 by etrobert          #+#    #+#             */
-/*   Updated: 2017/02/14 21:05:53 by etrobert         ###   ########.fr       */
+/*   Updated: 2017/02/14 23:36:43 by etrobert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "corewar.h"
 
-t_corewar			*corewar_new(void)
+t_corewar			*corewar_new(t_list *champions);
 {
 	t_corewar		*cw;
 
 	if ((cw = (t_corewar *)(malloc(sizeof(t_corewar)))) == NULL)
 		return (NULL);
-	if (corewar_init(cw) < 0)
+	if (corewar_init(cw, champions) < 0)
 	{
 		corewar_delete(cw);
 		return (NULL);
