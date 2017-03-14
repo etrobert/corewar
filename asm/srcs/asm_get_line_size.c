8a8d3a19f6c18e31bad79e765e35765bd14b05d8
Maxime VERDIER <mverdier@e2r11p23.42.fr>
2017-03-14 16:26:14 +0100

    handling empty file and empty name (juste .s) like zaz

--- asm/srcs/asm_get_line_size.c
+++ asm/srcs/asm_get_line_size.c
@@ -6,7 +6,7 @@
 /*   By: mverdier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/28 18:05:29 by mverdier          #+#    #+#             */
-/*   Updated: 2017/03/12 16:17:52 by mverdier         ###   ########.fr       */
+/*   Updated: 2017/03/14 16:19:57 by mverdier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,8 +54,7 @@ static int		asm_go_to_instruct_size(char **split, int *n, t_list **labels,
 	return (1);
 }
 
-static int		asm_check_name_comment_op(t_asm *m_asm, char **split, int n,
-		t_op **op_tab)
+static int		asm_check_name_and_comment(t_asm *m_asm, char **split)
 {
 	if (!m_asm->name || !m_asm->comment)
 	{
@@ -63,6 +62,11 @@ static int		asm_check_name_comment_op(t_asm *m_asm, char **split, int n,
 		asm_free_split(split);
 		return (0);
 	}
+	return (1);
+}
+
+static int		asm_check_op(char **split, int n, t_op **op_tab)
+{
 	if ((*op_tab = get_op_by_name(split[n])) == NULL)
 	{
 		ft_dprintf(2, "Bad op \'%s\'\n", split[n]);
@@ -86,10 +90,12 @@ unsigned int	asm_get_line_size(char *line, t_list **labels,
 		ft_dprintf(2, "Malloc error\n");
 		return (-1);
 	}
+	if (!asm_check_name_and_comment(m_asm, split))
+		return (-1);
 	n = 0;
 	if ((ret = asm_go_to_instruct_size(split, &n, labels, big_size)) < 1)
 		return (ret);
-	if (!asm_check_name_comment_op(m_asm, split, n, &op_tab))
+	if (!asm_check_op(split, n, &op_tab))
 		return (-1);
 	if ((size = asm_get_params_size(split, n + 1, op_tab)) > CHAMP_MAX_SIZE
 			|| !asm_check_separators(line, op_tab))
