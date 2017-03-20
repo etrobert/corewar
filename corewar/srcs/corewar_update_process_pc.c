df4e86e5c1495ff821d72843ffe9c04b8301a522
Etienne ROBERT <etrobert@e2r11p21.42.fr>
2017-03-20 20:44:08 +0100

    Small fixes

--- corewar/srcs/corewar_update_process_pc.c
+++ corewar/srcs/corewar_update_process_pc.c
@@ -6,7 +6,7 @@
 /*   By: etrobert <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/17 15:31:04 by etrobert          #+#    #+#             */
-/*   Updated: 2017/03/14 18:49:23 by etrobert         ###   ########.fr       */
+/*   Updated: 2017/03/20 20:28:12 by etrobert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,14 @@
 void				corewar_update_process_pc(t_corewar *corewar,
 		t_process *proc, int value)
 {
+	unsigned int	new_pc;
+
 	(void)corewar;
 	if (proc == NULL)
 		return ;
-	proc->pc = (proc->pc + value) % MEM_SIZE;
+	new_pc = (proc->pc + value) % MEM_SIZE;
+	if ((corewar->verbosity & CW_VB_PC_MOV) != 0)
+		corewar_print_log(corewar, "Process %u moved from %4u to %4u\n",
+				proc->proc_id, proc->pc, new_pc);
+	proc->pc = new_pc;
 }
