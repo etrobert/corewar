873c044d532e7d415945821bedd733d4d234eade
Etienne ROBERT <etrobert@e2r11p21.42.fr>
2017-03-20 21:23:04 +0100

    MOWGLI

--- champions/mowgli.s
+++ champions/mowgli.s
@@ -12,19 +12,77 @@ sti		r1, %:live, %1					; *(live + 1) = r1
 
 big_loop:
 	loopinit:
-	ld		%44, r3						; r3 = 44
+	ld		%64, r3						; r3 = 44
 
 	loop:
 		live:
 			live	%0
-		ldi		%:big_loop, r3, r6		; r6 = *(big_loop + r3)
-		sti		r6, %:end_big_loop, r3	; *(end_big_loop + r3) = r6
 		sub		r3, r5, r3				; r3 -= 4
+		ldi		%:big_loop, r3, r6		; r6 = *(big_loop + r3)
+		sti		r6, %:big_loop_end, r3	; *(end_big_loop + r3) = r6
 		zjmp	%:end_loop				; if (r3 == 0) stop loop
 		and		r1, %0, r9				; carry = 1
 		zjmp	%:loop					; goto loop
 
 	end_loop:
+	fork	%:big_loop_end
 	and		r1, %0, r9					; carry = 1
 	zjmp	%:big_loop					; goto big_loop
+
+	live	%0
+	live	%0
+	live	%0
+	live	%0
+	live	%0
+	live	%0
+	live	%0
+	live	%0
+	live	%0
+	live	%0
+	live	%0
+	live	%0
+	live	%0
+	live	%0
+	live	%0
+	live	%0
+	live	%0
+	live	%0
+	live	%0
+	live	%0
+	live	%0
+	live	%0
+	live	%0
+	live	%0
+	live	%0
+	live	%0
+	live	%0
+	live	%0
+	live	%0
+	live	%0
+	live	%0
+	live	%0
+	live	%0
+	live	%0
+	live	%0
+	live	%0
+	live	%0
+	live	%0
+	live	%0
+	live	%0
+	live	%0
+	live	%0
+	live	%0
+	live	%0
+	live	%0
+	live	%0
+	live	%0
+	live	%0
+	live	%0
+	live	%0
+	live	%0
+	live	%0
+	live	%0
+	live	%0
+	live	%0
+	live	%0
 big_loop_end:
