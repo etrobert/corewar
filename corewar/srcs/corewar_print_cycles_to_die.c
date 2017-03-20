c85f308deb040b2cf0c40189add1e93f19e7e840
Etienne ROBERT <etrobert@e2r11p21.42.fr>
2017-03-20 22:04:12 +0100

    corewar_print_cycles_to_die correction

--- corewar/srcs/corewar_print_cycles_to_die.c
+++ corewar/srcs/corewar_print_cycles_to_die.c
@@ -6,7 +6,7 @@
 /*   By: etrobert <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/15 18:02:50 by etrobert          #+#    #+#             */
-/*   Updated: 2017/03/20 20:42:50 by etrobert         ###   ########.fr       */
+/*   Updated: 2017/03/20 22:01:07 by etrobert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int					corewar_print_cycles_to_die(t_corewar *corewar)
 {
-	if ((corewar->verbosity & CW_VB_CYCLES) != 0)
+	if ((corewar->verbosity & CW_VB_CYCLES) == 0)
 		return (0);
 	return (corewar_print_log(corewar, "Cycle to die is now %u\n",
 				corewar->cycles_to_die));
