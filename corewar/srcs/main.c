ab6218c7acc52141d12ceb3f5bf4978f9d70805e
Maxime VERDIER <mverdier@e2r11p22.42.fr>
2017-03-20 22:09:34 +0100

    some small changes on visu

--- corewar/srcs/main.c
+++ corewar/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: etrobert <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/11 18:50:20 by etrobert          #+#    #+#             */
-/*   Updated: 2017/03/20 16:46:05 by mverdier         ###   ########.fr       */
+/*   Updated: 2017/03/20 22:08:54 by mverdier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,11 +32,29 @@ static void	free_params(t_list *champs, t_corewar *cw, t_parser *parser)
 	corewar_delete(cw);
 }
 
-int main(int argc, char **argv)
+static void	print_champs(t_list *champs)
+{
+	t_list_it	it;
+	t_champion	*champ;
+
+	it = ft_list_begin(champs);
+	ft_printf("%d champions loaded : \n", ft_list_size(champs));
+	while (!ft_list_it_end(champs, it))
+	{
+		champ = ft_list_it_get(champs, it);
+		ft_printf("  - Player %u, \"%s\", (\"%s\"), weighs %u\n", champ->id,
+				champ->header.prog_name, champ->header.comment,
+				champ->header.prog_size);
+		ft_list_it_inc(&it);
+	}
+}
+
+int			main(int argc, char **argv)
 {
 	t_list		*champs;
 	t_corewar	*cw;
 	t_parser	*parser;
+	t_champion	*winner;
 
 	if (!int_good_size())
 	{
@@ -51,27 +69,18 @@ int main(int argc, char **argv)
 	if (parse_args(parser, &champs) < 0)
 	{
 		free_params(champs, cw, parser);
-		//		parser_delete(parser);
 		return (-1);
 	}
 	if (champs == NULL || ft_list_size(champs) < 1)
 	{
 		ft_dprintf(2, "No champions loaded\n");
-		/*		parser_delete(parser);
-				ft_list_apply(champs, (t_f_apply)&champion_delete);
-				ft_list_delete(champs); */
 		free_params(champs, cw, parser);
 		return (-1);
 	}
-	ft_printf("%d champions loaded\n", ft_list_size(champs));
+	print_champs(champs);
 	if ((cw = corewar_new(champs, 2)) == NULL)
 	{
 		ft_dprintf(2, "Error creating corewar\n");
-		/*
-		   parser_delete(parser);
-		   ft_list_apply(champs, (t_f_apply)&champion_delete);
-		   ft_list_delete(champs);
-		   */
 		free_params(champs, cw, parser);
 		return (-1);
 	}
@@ -80,18 +89,15 @@ int main(int argc, char **argv)
 	if (play_corewar(cw, champs, parser) != 0)
 	{
 		ft_dprintf(2, "An error occured during the game\n");
-		/*		parser_delete(parser);
-				ft_list_apply(champs, (t_f_apply)&champion_delete);
-				ft_list_delete(champs); */
 		free_params(champs, cw, parser);
 		return (-1);
 	}
+	if (parser->dump_cycle == -1)
+	{
+		winner = corewar_get_winner(cw);
+		ft_printf("Player %u, \"%s\", has won !\n", winner->id,
+				winner->header.prog_name);
+	}
 	free_params(champs, cw, parser);
-	/*
-	   parser_delete(parser);
-	   ft_list_apply(champs, (t_f_apply)&champion_delete);
-	   ft_list_delete(champs);
-	   corewar_delete(cw);
-	   */
 	return (0);
 }
